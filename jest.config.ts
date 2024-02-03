f5c025623bd4eb0822af3657f23df6e01fb2691f
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-02-03 16:30:43 +0900

    [add] jest config

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+        },
+      },
+    ],
+    "^.+\\.(js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+};
+
+export default jestConfig;
