4a560401a5fb86777e3709d760c8adbeb1e55ec2
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2024-02-16 18:53:11 +0900

    [fix] test

--- src/lib/task/__tests__/getSubmissionCounts.test.ts
+++ src/lib/task/__tests__/getSubmissionCounts.test.ts
@@ -28,6 +28,7 @@ describe("getSubmissionCounts", () => {
     const condition = {
       ...defaultCondition(),
       beginDateTime: dayjs(0),
+      crawlBeginDateTime: dayjs(0),
       endDateTime: dayjs(1704034800000),
     };
     const task = {
